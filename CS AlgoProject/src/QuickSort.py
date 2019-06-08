0bbaacfe541fb5feb11893fa90db5beac09705ce
thevirtuoso1973 <chris.tomy@hotmail.com>
2019-06-08 09:34:48 +0100

    Add comments

--- CS AlgoProject/src/QuickSort.py
+++ CS AlgoProject/src/QuickSort.py
@@ -1,3 +1,4 @@
+# uses python list manipulation:
 def quickSort(L):
     # if there's less than two items it's already sorted
     if len(L) <= 1:
@@ -6,13 +7,13 @@ def quickSort(L):
     i, j = 0, len(L)-1
     while i != j:
         if i < j:
-            if L[i] > L[j]:
+            if L[i] > L[j]: # swap:
                 L[i], L[j] = L[j], L[i]
                 i, j = j, i
             else:
                 i += 1
         else:
-            if L[i] < L[j]:
+            if L[i] < L[j]: # swap:
                 L[i], L[j] = L[j], L[i]
                 i, j = j, i
             else:
