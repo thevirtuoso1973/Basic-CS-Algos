3b7225fe247022b1d7371c4da641688a564627d4
thevirtuoso1973 <chris.tomy@hotmail.com>
2019-06-08 15:40:23 +0100

    add methods called 'delete' and 'add' to linked list class

--- CS AlgoProject/src/LinkedList.py
+++ CS AlgoProject/src/LinkedList.py
@@ -12,3 +12,29 @@ class LinkedList:
     def __init__(self, headVal):
         self.head = Node(headVal)
         #self.tail = etc..
+
+    # adds a value in the correct position
+    def add(val):
+        curr = self.head
+        if val < curr.val:
+            self.head = Node(val)
+            self.head.next = curr
+        else:
+            while curr and val >= curr.val:
+                prev = curr
+                curr = curr.next
+            prev.next = Node(val)
+            prev.next.next = curr
+
+    # deletes a node with the value given
+    def delete(val):
+        curr = self.head
+        if val == curr.val:
+            self.head = curr.next
+        else:
+            while curr.next and curr.next.val != val:
+                curr = curr.next
+            if curr.next:
+                curr.next = curr.next.next
+            else:
+                print("Value not in linked list.")
